f4936d7918fda02f48d7695af5d0066119de52b9
haole <leanhhao86@gmail.com>
2020-07-09 19:44:03 +0700

    Backend immplementation

---
+++ gomoku/client.py
@@ -0,0 +1,34 @@
+import socketio
+import requests
+import sys
+
+sio = socketio.Client()
+
+def register():
+	url = "http://localhost:5000/register"
+	payload = {"username":"haole", "email":"hello@gmail.com", "password":"leanhhao" }
+
+	print(requests.post(url, data=payload).text)
+
+def connect(username, password):
+	header = {'username': username, 'password':password}    
+	sio.connect('http://localhost:5000', headers=header)
+
+def create_room(roomid):
+	sio.emit('create room', {'data': roomid})
+
+
+def join(roomid):
+	sio.emit('join', {'data':roomid})
+
+def leave():
+	sio.emit('leave', {})
+
+def makemove(x, y):
+	sio.emit('move', {'x':x, 'y': y})
+
+@sio.on('message')
+def response(message):
+    print(message)
+
+	
